6c356f9ee556085fe08cafea162d95701da73598
Lukas <lukasrys04@gmail.com>
2026-08-15 14:30:55 +0200

    Added an internal clock in pygame

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt: float = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -13,6 +15,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        print(dt)
         pygame.display.flip()
 
 
