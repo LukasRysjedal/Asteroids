25d0a73712057721d4bfd37f5d97f35ca5f83564
Lukas <lukasrys04@gmail.com>
2026-08-15 16:08:20 +0200

    Added a player.py, and function to draw player to screen

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
+#Screen variables
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+#Player variables
+PLAYER_RADIUS = 20
+LINE_WIDTH = 2
--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    #Clock and delta time
     clock = pygame.time.Clock()
     dt: float = 0.0
+
+    #screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         dt = clock.tick(60) / 1000
-        print(dt)
         pygame.display.flip()
 
 
