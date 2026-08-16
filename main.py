dda24b1a7ea184e227962ed0fda5215ace668646
Lukas <lukasrys04@gmail.com>
2026-08-16 14:02:15 +0200

    Added colision with asteroids and shots

--- main.py
+++ main.py
@@ -51,6 +51,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
         for object in drawable:
             object.draw(screen)
         pygame.display.flip()
